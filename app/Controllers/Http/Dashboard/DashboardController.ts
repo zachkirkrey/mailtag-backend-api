b6f765cea98aae603600a2dcc41d27a48aa770d5
bigdai21 <tolochko228@gmail.com>
2022-06-21 23:59:20 +0300

    Add dashboard get emails sent this month

--- app/Controllers/Http/Dashboard/DashboardController.ts
+++ app/Controllers/Http/Dashboard/DashboardController.ts
@@ -1,15 +1,30 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-import { daysAgo } from 'App/Helpers/date'
+import { daysAgo, monthsAgo } from 'App/Helpers/date'
 import Email from 'App/Models/Email'
 
 export default class DashboardController {
   public async getEmailsSentToday({ auth }: HttpContextContract) {
     const user = auth.use('api').user!
+    // TODO query based on this day not bigger than a day ago
     const oneDayAgo = daysAgo(1).toSQLDate()
     const emails = await Email.query()
       .where({ userId: user.id })
       .andWhere('created_at', '>=', oneDayAgo)
+
+    return {
+      data: {
+        emails,
+      },
+    }
+  }
+
+  public async getEmailsSentThisMonth({ auth }: HttpContextContract) {
+    const user = auth.use('api').user!
+    const oneMonthAgo = monthsAgo(1).toSQLDate()
+    const emails = await Email.query()
+      .where({ userId: user.id })
+      .andWhere('created_at', '>=', oneMonthAgo)
 
     return {
       data: {
--- app/Helpers/date.ts
+++ app/Helpers/date.ts
@@ -4,3 +4,8 @@ export const daysAgo = (days: number) => {
   const now = DateTime.now()
   return now.minus(Duration.fromObject({ days }))
 }
+
+export const monthsAgo = (months: number) => {
+  const now = DateTime.now()
+  return now.minus(Duration.fromObject({ months }))
+}
