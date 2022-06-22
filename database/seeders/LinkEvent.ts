6c524457696187671d4076a01140c73a82a32067
bigdai21 <tolochko228@gmail.com>
2022-06-22 23:12:54 +0300

    Add link event seeder

--- database/seeders/LinkEvent.ts
+++ database/seeders/LinkEvent.ts
@@ -1,7 +1,26 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import { TEST_USER_PROVIDER_ID } from 'App/Helpers/constant'
+import { LinkEventAttributes } from 'App/Helpers/type'
+import Link from 'App/Models/Link'
+import LinkEvent from 'App/Models/LinkEvent'
 
 export default class extends BaseSeeder {
   public async run() {
     // Write your database queries inside the run method
+    const link = await Link.query()
+      .whereHas('email', (query) =>
+        query.whereHas('user', (query) => query.where({ providerId: TEST_USER_PROVIDER_ID }))
+      )
+      .firstOrFail()
+
+    const linkEvents = Array.from(Array(5)).map(() => {
+      const linkEventAttributes: LinkEventAttributes = {
+        linkId: link.id,
+      }
+
+      return linkEventAttributes
+    })
+
+    await LinkEvent.createMany(linkEvents)
   }
 }
