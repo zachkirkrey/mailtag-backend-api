ad380e55e1484134c925274f085bd518d7f6175f
bigdai21 <tolochko228@gmail.com>
2022-07-22 18:10:20 +0300

    Remove redundant get team by id validator

--- start/routes/team/team.ts
+++ start/routes/team/team.ts
@@ -3,10 +3,9 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.get('', 'Team/TeamController.show')
   Route.post('', 'Team/TeamController.create')
-  Route.patch(':id', 'Team/TeamController.update')
-  Route.delete(':id', 'Team/TeamController.destroy')
+  Route.patch('', 'Team/TeamController.update')
+  Route.delete('', 'Team/TeamController.destroy')
   Route.get('stats', 'Team/TeamController.stats')
 })
   .prefix('team')
   .middleware('auth')
-  .where('id', Route.matchers.uuid())
