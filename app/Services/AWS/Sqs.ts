daa010a7e3671c0b0c0449b16cf4cd91479588a8
Pavel Khodakovsky <pavel.khodakovsky1996@gmail.com>
2022-08-04 20:23:50 +0300

    Fix sqs service sending requests at local development (#32)

--- app/Services/AWS/Sqs.ts
+++ app/Services/AWS/Sqs.ts
@@ -6,22 +6,40 @@ import User from 'App/Models/User'
 import WelcomeUserMail from 'App/Mailers/WelcomeUserMail'
 import { SQSMessageTypes } from 'App/Helpers/type'
 import PaymentException from 'App/Exceptions/PaymentException'
+import Application from '@ioc:Adonis/Core/Application'
+
+const SQS_AUTH_ERROR_MESSAGE =
+  'SQS receive message failed: The security token included in the request is invalid.'
 
 export default class Sqs {
-  private queueUrl = Env.get('SQS_QUEUE_URL')
-  private accessKeyId = Env.get('AWS_ACCESS_KEY')
-  private secretAccessKey = Env.get('AWS_ACCESS_SECRET')
-  private region = this.queueUrl.split('//')[1].split('.')[1]
-  private credentials = new Credentials({
-    accessKeyId: this.accessKeyId,
-    secretAccessKey: this.secretAccessKey,
+  private readonly queueUrl = Env.get('SQS_QUEUE_URL')
+  private readonly region = this.queueUrl.split('//')[1].split('.')[1]
+  private readonly credentials = new Credentials({
+    accessKeyId: Env.get('AWS_ACCESS_KEY'),
+    secretAccessKey: Env.get('AWS_ACCESS_SECRET'),
   })
-  private sqs = new SQS({
+  private readonly sqs = new SQS({
     region: this.region,
     credentials: this.credentials,
   })
+  private readonly consumer: Consumer
+
+  constructor() {
+    this.consumer = Consumer.create({
+      queueUrl: this.queueUrl,
+      handleMessage: this.handleMessage,
+      messageAttributeNames: ['All'],
+    })
+
+    this.attachConsumerEventListeners()
+  }
 
   public async sendMessage(type: string, userId: User['id'], email: User['email']) {
+    if (Application.inDev) {
+      Logger.info(JSON.stringify({ SqsMessage: { type, userId, email } }, null, 2))
+      return
+    }
+
     const params = {
       QueueUrl: this.queueUrl,
       MessageBody: `${type}/${userId}/${email}`,
@@ -56,21 +74,7 @@ export default class Sqs {
   }
 
   public async start() {
-    const app = Consumer.create({
-      queueUrl: this.queueUrl,
-      handleMessage: this.handleMessage,
-      messageAttributeNames: ['All'],
-    })
-
-    app.on('error', (err) => {
-      Logger.error(err.message)
-    })
-
-    app.on('processing_error', (err) => {
-      Logger.error(err.message)
-    })
-
-    app.start()
+    this.consumer.start()
   }
 
   public async handleMessage(message: SQS.Message) {
@@ -86,5 +90,20 @@ export default class Sqs {
       default:
         throw new PaymentException('No message type', 422)
     }
+  }
+
+  private attachConsumerEventListeners() {
+    this.consumer.on('error', async (err) => {
+      Logger.error(err.message)
+      if (err.message === SQS_AUTH_ERROR_MESSAGE) {
+        // TODO: crash the process consumer is running on
+        Logger.error('Stopping the sqs consumer, please fix your auth token')
+        this.consumer.stop()
+      }
+    })
+
+    this.consumer.on('processing_error', (err) => {
+      Logger.error(err.message)
+    })
   }
 }
--- start/events.ts
+++ start/events.ts
@@ -1,15 +1,3 @@
-// import Event from '@ioc:Adonis/Core/Event'
-// import Logger from '@ioc:Adonis/Core/Logger'
-// import Database, { DbQueryEventNode } from '@ioc:Adonis/Lucid/Database'
-// import Application from '@ioc:Adonis/Core/Application'
-
-// Event.on('db:query', (query: DbQueryEventNode) => {
-//   if (Application.inProduction) {
-//     Logger.debug(query)
-//   } else {
-//     Database.prettyPrint(query)
-//   }
-// })
 import Event from '@ioc:Adonis/Core/Event'
 import Database from '@ioc:Adonis/Lucid/Database'
 
