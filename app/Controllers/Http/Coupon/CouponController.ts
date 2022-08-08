87ede5c93e8180adf329d6edf423b056018bcfe5
Bryson <sbryson630@gmail.com>
2022-08-08 16:27:22 +0700

    Add coupon pagination

--- app/Controllers/Http/Coupon/CouponController.ts
+++ app/Controllers/Http/Coupon/CouponController.ts
@@ -4,17 +4,15 @@ import User from 'App/Models/User'
 import CreateCouponValidator from 'App/Validators/Coupon/CreateCouponValidator'
 import GetCouponByIdValidator from 'App/Validators/Coupon/GetCouponByIdValidator'
 import UpdateCouponValidator from 'App/Validators/Coupon/UpdateCouponValidator'
+import Config from '@ioc:Adonis/Core/Config'
 
 export default class CouponController {
-  public async index({ auth }: HttpContextContract) {
+  public async index({ auth, request }: HttpContextContract) {
     const user: User = auth.use('api').user!
-    const coupons = await Coupon.query().where({ userId: user.id })
-
-    return {
-      data: {
-        coupons: coupons.map((coupon) => coupon.serializedCouponInfo),
-      },
-    }
+    const page: number = request.input('page', Config.get('app.pagination.page'))
+    const limit: number = request.input('limit', Config.get('app.pagination.limit'))
+    const coupons = await Coupon.query().where({ userId: user.id }).paginate(page, limit)
+    return coupons.serialize()
   }
 
   public async show({ auth, request }: HttpContextContract) {
@@ -24,7 +22,7 @@ export default class CouponController {
 
     return {
       data: {
-        coupon: coupon.serializedCouponInfo,
+        coupon: coupon.serialize(),
       },
     }
   }
@@ -42,7 +40,7 @@ export default class CouponController {
     return {
       data: {
         message: 'Coupon created successfully',
-        coupon: coupon.serializedCouponInfo,
+        coupon: coupon.serialize(),
       },
     }
   }
@@ -57,7 +55,7 @@ export default class CouponController {
     return {
       data: {
         message: 'Coupon updated successfully',
-        coupon: updatedCoupon.serializedCouponInfo,
+        coupon: updatedCoupon.serialize(),
       },
     }
   }
